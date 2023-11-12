e91c86137053d1d5c9ed9e375a3e2dfb557a423c
leedongyeop <ldy_1204@naver.com>
2023-11-12 14:37:55 +0900

    chore : project setting

---
+++ src/main/java/io/dongvelop/springbootquerydsltutorial/SpringbootQuerydslTutorialApplication.java
@@ -0,0 +1,13 @@
+package io.dongvelop.springbootquerydsltutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootQuerydslTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootQuerydslTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/dongvelop/springbootquerydsltutorial/SpringbootQuerydslTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dongvelop.springbootquerydsltutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootQuerydslTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
